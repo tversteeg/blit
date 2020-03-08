136f72ef2c39c7c833629c9f3abc5f8df6aad8c0
Thomas Versteeg <thomasversteeg@gmx.com>
2020-03-08 15:48:43 +0100

    Change benchmark suite to criterion

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,87 +1,66 @@
-#![feature(test)]
-
-extern crate blit;
-extern crate image;
-extern crate test;
-
-use test::Bencher;
-
 use blit::*;
+use criterion::{criterion_group, criterion_main, Criterion};
 
 const SIZE: usize = 1000;
-const ITERATIONS: i32 = 100;
+const ITERATIONS: i32 = 10;
 
-#[bench]
-fn blit_buffer(b: &mut Bencher) {
-    let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
+fn criterion_benchmark(c: &mut Criterion) {
+    let img_rgb = image::open("examples/smiley_rgb.png").unwrap();
+    let rgb = img_rgb.as_rgb8().unwrap();
+    let img_rgba = image::open("examples/smiley_rgba.png").unwrap();
+    let rgba = img_rgba.as_rgba8().unwrap();
 
-    let img = image::open("examples/smiley_rgb.png").unwrap();
-    let rgb = img.as_rgb8().unwrap();
-    let blit = rgb.to_blit_buffer(Color::from_u32(0xFF00FF));
-
-    b.iter(|| {
-        for x in 0..ITERATIONS {
-            blit.blit(&mut buffer, SIZE, (x, 0));
-        }
-    });
-}
-
-#[bench]
-fn blit_buffer_rect(b: &mut Bencher) {
-    let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
-
-    let img = image::open("examples/smiley_rgb.png").unwrap();
-    let rgb = img.as_rgb8().unwrap();
     let blit = rgb.to_blit_buffer(Color::from_u32(0xFF00FF));
     let size = blit.size();
 
-    b.iter(|| {
-        for x in 0..ITERATIONS {
-            blit.blit_rect(&mut buffer, SIZE, (x, 0), (0, 0, size.0, size.1));
-        }
+    c.bench_function("buffer", |b| {
+        let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
+
+        b.iter(|| {
+            for x in 0..ITERATIONS {
+                blit.blit(&mut buffer, SIZE, (x * 100, 0));
+            }
+        });
     });
-}
 
-#[bench]
-fn blit_buffer_exact_fit(b: &mut Bencher) {
-    let img = image::open("examples/smiley_rgb.png").unwrap();
-    let rgb = img.as_rgb8().unwrap();
-    let blit = rgb.to_blit_buffer(Color::from_u32(0xFF00FF));
+    c.bench_function("buffer rect", |b| {
+        let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
 
-    let size = blit.size();
-    let mut buffer: Vec<u32> = vec![0; (size.0 * size.1) as usize];
+        b.iter(|| {
+            for x in 0..ITERATIONS {
+                blit.blit_rect(&mut buffer, SIZE, (x * 100, 0), (0, 0, size.0, size.1));
+            }
+        });
+    });
 
-    b.iter(|| {
-        for _ in 0..ITERATIONS {
+    c.bench_function("exact fit", |b| {
+        let mut buffer: Vec<u32> = vec![0; (size.0 * size.1) as usize];
+
+        b.iter(|| {
             blit.blit(&mut buffer, SIZE, (0, 0));
-        }
+        });
+    });
+
+    c.bench_function("img rgb", |b| {
+        let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
+
+        b.iter(|| {
+            for x in 0..ITERATIONS {
+                rgb.blit(&mut buffer, SIZE, (x * 100, 0), Color::from_u32(0xFF00FF));
+            }
+        });
+    });
+
+    c.bench_function("img rgba", |b| {
+        let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
+
+        b.iter(|| {
+            for x in 0..ITERATIONS {
+                rgba.blit(&mut buffer, SIZE, (x * 100, 0), Color::from_u32(0xFF00FF));
+            }
+        });
     });
 }
 
-#[bench]
-fn blit_img_rgb(b: &mut Bencher) {
-    let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
-
-    let img = image::open("examples/smiley_rgb.png").unwrap();
-    let rgb = img.as_rgb8().unwrap();
-
-    b.iter(|| {
-        for x in 0..ITERATIONS {
-            rgb.blit(&mut buffer, SIZE, (x, 0), Color::from_u32(0xFF00FF));
-        }
-    });
-}
-
-#[bench]
-fn blit_img_rgba(b: &mut Bencher) {
-    let mut buffer: Vec<u32> = vec![0; SIZE * SIZE];
-
-    let img = image::open("examples/smiley_rgba.png").unwrap();
-    let rgb = img.as_rgba8().unwrap();
-
-    b.iter(|| {
-        for x in 0..ITERATIONS {
-            rgb.blit(&mut buffer, SIZE, (x, 0), Color::from_u32(0xFF00FF));
-        }
-    });
-}
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
